debb8b467b22efff722538eaa76d068dc5fc857c
David Peter Horvath <hhorvath.david.peter@gmail.com>
2025-11-27 23:19:32 +0100

    cím hozzáadása

--- hello-devops-react/src/App.js
+++ hello-devops-react/src/App.js
@@ -3,8 +3,7 @@ import './App.css';
 function App() {
   return (
     <div style={{ fontSize: "32px", padding: "40px" }}>
-      Update!
-      Hello DevOps!
+      <h3>Üdv a React és DevOps világában!</h3>
     </div>
   );
 }
